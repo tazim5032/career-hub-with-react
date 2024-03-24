e70c5d9e405fb4521586f601947399a1a1ac9620
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-03-25 03:35:53 +0600

    buttons added

--- src/FeaturedJobs.jsx
+++ src/FeaturedJobs.jsx
@@ -2,10 +2,9 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Job from "./Job";
 
-
-
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+    const [dataLength, setDatalength] = useState(4); //initially 4 ta data dekhabo tai
 
     useEffect(() =>{
         fetch('./jobs.json')
@@ -18,10 +17,14 @@ const FeaturedJobs = () => {
                 <h2 className="text-5xl text-center">Featured jobs : {jobs.length}</h2>
                 <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, voluptatibus.</p>
             </div>
-            <div className="grid grid-cols-2 gap-6">
+            <div className="grid grid-cols-2 gap-6 mb-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job> )
+                    jobs.slice(0,dataLength).map(job => <Job key={job.id} job={job}></Job> )
                 }
+            </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                <button onClick={() => setDatalength(jobs.length)}
+                 className="btn btn-primary">Show ALl Jobs</button>
             </div>
         </div>
     );
--- src/Job.jsx
+++ src/Job.jsx
@@ -1,3 +1,6 @@
+import { MdLocationOn } from "react-icons/md";
+import { CiDollar } from "react-icons/ci";
+
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
@@ -10,6 +13,10 @@ const Job = ({ job }) => {
                     <button className="px-5 py-2 text-[#7E90FE] font-bold border rounded border-[#7E90FE] mr-4">{remote_or_onsite}</button>
                     <button className="px-5 py-2 text-[#7E90FE] font-bold border rounded border-[#7E90FE]">{job_type}</button>
                 </div >
+                <div className="mt-4 flex gap-2">
+                    <h1 className="flex"><MdLocationOn className="text-2xl mr-1"></MdLocationOn>{location}</h1>
+                    <h1 className="flex"> <CiDollar className="text-2xl mr-1"></CiDollar>{salary} </h1>
+                </div>
                 <div className="card-actions">
                     <button className="btn btn-primary">View Details</button>
                 </div>
